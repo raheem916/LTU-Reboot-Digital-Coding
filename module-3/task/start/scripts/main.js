68496012b4a56b2c1e4aa3b76d16fd0bf7e523a8
pixel2code <luke@pixeltocode.co>
2021-10-05 20:57:14 +0100

    Update main.js
    Add practice comments, Variables, console.log's, document.write and datatypes

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,55 @@
+// This is a single line comment
+
+/**
+ *  This is a multiple
+ *  line comment
+ * */
+
+/**
+ *    let age = 37;
+ *    const name = 'Luke';
+ *    var address = '123 long avenue';
+ * 
+    *  let $variable = 0;
+    *  let variable = 0;
+    *  let _variable = 0;
+    * 
+    * let firstName = "Luke";
+    * let lastName = "Ashton-Johnson";
+ */
+
+const productName = 'Baked Beans';
+let price = 1.27;
+let quantity = 4;
+let inStock = true;
+let discountAmount = 10;
+
+// console.log( 'Product Name:', productName );
+// console.log( 'Price:',price );
+// console.log( 'Quantity:', quantity );
+// console.log( 'In Stock:',inStock );
+// console.log( 'Discount:', discountAmount );
+
+// console.log( typeof productName );
+// console.log( typeof price );
+// console.log( typeof quantity );
+// console.log( typeof inStock );
+// console.log( typeof discountAmount );
+ 
+// document.write( "<p>" + productName + "</p>");
+
+// let myName = '';
+// let numOfKids = 0;
+// let isMarried;
+
+// let newName = 'Luke';
+// let newNumOfKids = '1';
+
+// console.log( Boolean(myName) );
+// console.log( Boolean(numOfKids) );
+// console.log( Boolean(isMarried) );
+// console.log( Boolean(null) );
+// console.log( Boolean(undefined) );
+// console.log( '--------')
+// console.log( Boolean(newName) );
+// console.log( Boolean(newNumOfKids) );
