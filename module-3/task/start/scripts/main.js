5dbe4ee8a0b58d610ad6053aa2a3ef4376eddc29
pixel2code <luke@pixeltocode.co>
2021-10-14 20:58:50 +0100

    Update main.js
    Completed tasks from task 4 - 8

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -53,3 +53,100 @@ let discountAmount = 10;
 // console.log( '--------')
 // console.log( Boolean(newName) );
 // console.log( Boolean(newNumOfKids) );
+
+function showProductName() {
+   console.log( productName );
+}
+
+function totalPrice( productPrice,productQuantity ) {
+   console.log( productPrice * productQuantity );
+}
+
+// totalPrice( price, quantity );
+
+// function checkAvailability( productInStock ) {
+//    console.log( productInStock );
+// }
+
+const checkAvailability = productInStock => console.log( productInStock );
+
+// checkAvailability( inStock );
+
+// let global = 'Global scope';
+
+// const localScope = () => {
+//    let scope = 'Local Scope'; // Local
+//    console.log(`Inside the function localScope: ${scope}`);
+//    console.log(`From outside the function : ${global}`);
+// }
+
+// localScope();
+
+// console.log( global );
+
+const productDiscount = () => {
+   if( quantity > 1 ) {
+      let newPrice = discountAmount - ( price * quantity );
+      console.log( newPrice );
+   } else {
+      console.log( price * quantity );
+   }
+}
+
+// productDiscount();
+
+
+// switch Statement
+
+// const level = 6;
+
+// switch( level ) {
+//    case 1 :
+//       console.log('You are on level 1');
+//       break;
+//    case 2 :
+//       console.log('You are on level 2');
+//       break;
+//    case 3 :
+//       console.log('You are on level 3');
+//       break;
+//    case 4 :
+//       console.log('You are on level 4');
+//       break;
+//    default:
+//       console.log('No level found');
+//       break;
+// }
+
+// if( level === 1 ) {
+//    console.log('You are on level 1');
+// } else if(level === 2) {
+//    console.log('You are on level 2');
+// } //....
+// else {
+//    console.log('No level found');
+// }
+
+// ....... code
+
+
+const drinkOrder = ( drink, size ) => {
+
+   switch( drink ) {
+      case 'cola':
+         console.log( `${size} : ${drink}`);
+         break;
+      case 'lemonade':
+         console.log( `${size} : ${drink}`);
+         break;
+      case 'orangeade':
+         console.log( `${size} : ${drink}`);
+         break;
+      default:
+         console.log(`We currently don't have your ${drink} in stock`);
+         break;
+   }
+
+}
+
+drinkOrder('cola', 'x-large');
