98725516c2ae0bf1f9acb64e3ed06755791ef835
raheem916 <92947997+raheem916@users.noreply.github.com>
2021-11-11 21:04:30 +0000

    Added Java script

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,23 @@
+console.log('Hello');
+
+//single line comment
+
+/*------
+
+
+multi line comment
+
+-------*/
+
+let productName = 'raheem';
+let price = '10.00';
+let quantity = 3;
+let inStock = true;
+let discountAmount= 0;
+
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);
