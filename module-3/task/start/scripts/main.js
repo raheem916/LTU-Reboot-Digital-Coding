6bc74dd9c9d253ec5c2c16aefcdb54523bf4bfc3
raheem916 <92947997+raheem916@users.noreply.github.com>
2021-11-16 21:04:51 +0000

    updated discount tracker

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -13,7 +13,7 @@ let productName = 'raheem';
 let price = '10.00';
 let quantity = 3;
 let inStock = true;
-let discountAmount= 0;
+let discountAmount= 2;
 
 
 console.log(productName);
@@ -21,3 +21,46 @@ console.log(price);
 console.log(quantity);
 console.log(inStock);
 console.log(discountAmount);
+
+
+function showProductName(){
+    console.log (productName);
+}
+
+showProductName();
+
+function totalPrice(productPrice=0,productQuantity=0){
+    productPrice = parseFloat (productPrice);
+    productQuantity = parseFloat(productQuantity);
+    let totalProductPrice = productPrice * productQuantity; 
+    console.log(totalProductPrice);
+}
+
+totalPrice(price,quantity);
+
+function squareNumber(number=0){
+    let sum = number* number;
+    return sum;
+}
+
+var square = number => number * number;
+
+let squaredNumber = squareNumber(3);
+console.log (squaredNumber);
+
+let squared = square(3);
+console.log(squared);
+
+function productDiscount(){
+    let sum;
+    if(quantity > 1){
+        sum=price * quantity;
+        let discount = discountAmount * quantity;
+        sum = sum - discount;
+    } else { 
+        sum = price * quantity;
+    }
+    console.log(sum);
+}
+
+productDiscount(); 
